e381cde20980f176393108c46351ddb82c2fe0bf
han <roshanyjiand@gmail.com>
2026-08-22 16:39:38 +0800

    multidimensional_arrays completed

---
+++ pythonProjecct/Numpy/multidimensional_arrays.py
@@ -0,0 +1,28 @@
+# 多维数组
+import numpy as np
+
+array = np.array('A')
+print(array)
+print(array.ndim)
+
+array = np.array(['A'])
+print(array)
+print(array.ndim)
+
+array = np.array([['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']])
+print(array)
+print(array.ndim)
+
+array = np.array([[['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']],
+                  [['J', 'K', 'L'], ['M', 'N', 'O'], ['P', 'Q', 'R']],
+                  [['S', 'T', 'U'], ['V', 'W', 'X'], ['Y', 'Z', '0']]])
+
+word = array[0, 0, 0] + array[2, 0, 0] + array[2, 0, 0]
+
+print(array)
+print(array[0][0][0])
+print(array[0, 0, 0]) #更快
+print(word)
+print(array.ndim)
+print(array.shape)
+
